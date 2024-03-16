39c7c68cda25ac51c4be854e7340eb2e584b57fe
Denisa Kita <denisakita7@gmail.com>
2024-03-16 17:45:29 +0100

    add nav bar component

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -5,12 +5,14 @@ import {ListingDataFormComponent} from "./listing-data-form/listing-data-form.co
 import {ListingsPageComponent} from "./listings-page/listings-page.component";
 import {MyListingPageComponent} from "./my-listing-page/my-listing-page.component";
 import {NewListingPageComponent} from "./new-listing-page/new-listing-page.component";
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
 
 
 export const components: any[] = [
   ContactPageComponent, EditListingPageComponent, ListingDataFormComponent,
   ListingDetailPageComponent, ListingsPageComponent,
-  MyListingPageComponent, NewListingPageComponent
+  MyListingPageComponent, NewListingPageComponent,
+  NavBarComponent
 ]
 
 export * from './contact-page/contact-page.component';
@@ -20,4 +22,5 @@ export * from './listing-detail-page/listing-detail-page.component';
 export * from './listings-page/listings-page.component';
 export * from './my-listing-page/my-listing-page.component';
 export * from './new-listing-page/new-listing-page.component';
+export * from './nav-bar/nav-bar.component';
 
