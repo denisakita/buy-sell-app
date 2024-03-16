ab1bcbe5bae01c3a25db81de25fef8fb5c0f6148
Denisa Kita <denisakita7@gmail.com>
2024-03-16 15:05:07 +0100

    create listing data form

--- src/app/components/listing-data-form/listing-data-form.component.ts
+++ src/app/components/listing-data-form/listing-data-form.component.ts
@@ -1,10 +1,39 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Listing} from "../../models/types";
 
 @Component({
   selector: 'app-listing-data-form',
   templateUrl: './listing-data-form.component.html',
   styleUrl: './listing-data-form.component.css'
 })
-export class ListingDataFormComponent {
+export class ListingDataFormComponent implements OnInit {
+  listingFG: FormGroup = new FormGroup({})
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+  ) {
+  }
+
+  ngOnInit() {
+    this.listingFG = this.fb.group({
+      name: ['', Validators.required],
+      description: [''],
+      price: [0, Validators.min(0)]
+    });
+  }
+
+  onSubmit() {
+    if (this.listingFG.valid) {
+      const newListing: Listing = this.listingFG.value;
+
+      alert('Edit listing...');
+      this.router.navigateByUrl('/my-listings');
+    } else {
+      alert('Please fill out all required fields.');
+    }
+  }
 
 }
