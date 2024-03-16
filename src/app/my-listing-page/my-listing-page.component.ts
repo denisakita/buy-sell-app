1e321567af6854204872e859f97156a840dab4aa
Denisa Kita <denisakita7@gmail.com>
2024-03-16 14:31:33 +0100

    create my listing page

--- src/app/my-listing-page/my-listing-page.component.ts
+++ src/app/my-listing-page/my-listing-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Listing} from "../models/types";
+import {fakeListing} from "../../assets/mock/fake-data";
 
 @Component({
   selector: 'app-my-listing-page',
   templateUrl: './my-listing-page.component.html',
   styleUrl: './my-listing-page.component.css'
 })
-export class MyListingPageComponent {
+export class MyListingPageComponent implements OnInit {
+  listings:Listing[] =[];
+  constructor() {
+  }
 
+  ngOnInit() {
+    this.listings = fakeListing;
+  }
+
+  onDeleteClicked(listingId:string){
+    alert(`Deleting your listing with id ${listingId}`);
+}
 }
--- src/app/new-listing-page/new-listing-page.component.ts
+++ src/app/new-listing-page/new-listing-page.component.ts
@@ -1,10 +1,16 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-new-listing-page',
   templateUrl: './new-listing-page.component.html',
   styleUrl: './new-listing-page.component.css'
 })
-export class NewListingPageComponent {
+export class NewListingPageComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
 
 }
